9ad44113cc3af8bbc2d0a5cfc7d7c3b9b035172f
AdiWeit <43521682+AdiWeit@users.noreply.github.com>
2023-01-15 13:15:53 +0100

    :sparkles:audio inhibit setting:bulb:compatibility comment

--- settings.py
+++ settings.py
@@ -7,6 +7,8 @@ inhibit_percentage = 80
 # locks the screen when the PC normally goes to sleep. 
 # If it is set to True, it directly inhibits the PC. If set to False, it will set the inactivity time until going to sleep to never. 
 # If the PC is set to enter the lockscreen (setting in screensaver settings menu) it will still do that if keep_screen_on is False. 
+# Setting it to True may be necessary if you are not running Cinnamon. Otherwise, you would have to search for other commands to edit the power settings on your system. 
+# If you find other commands that work for your system, it would be great if you would open a pull request so it could be integrated for everyone!
 lock_screen = False
 # inhibits the black screen (lock_screen must be set to False)
 keep_screen_on = False
@@ -19,6 +21,10 @@ default_time_inactive = {"screen": {"decharging": 300, "charging": 300}, "sleep"
 # In the example below you have "Telegram" as a porgram name (application name or class_group_name). 
 # the 2 behind the "count" means, that the PC will inhibit, if the application ("Telegram") has 2 or more windows open
 # the "Vorschau" behind "title" of the "Anki" application means, that the PC will inhibit, if a window named "Vorschau" of the application named "Anki" is open
-# to check out how your open windows (applications) are called, you can uncommennt print lines in the "windows_filter.py" file (the first print is the following: "print("new window:")")
+# to check out how your open windows (applications) are called, you can uncommennt print lines in the "windows_and_audio_filter.py" file (the first print is the following: "print("new window:")")
 # info: the application title and the window title specified here only have to be part of the real title. 
 window_filter = {"Telegram": {"count": 2}, "Audacity": {"count": 1}, "Audio": {"title": "Audio-Rekorder"}, "cinnamon-session": {"title": "Sitzung"}}
+# all: all detected audio playing inhibits the PC
+# none: audio playing does not inhibit the PC
+# ["application1", "application2"]: inhibit if audio comes form specified application (application1 or application2 in this example)
+audio_filter = "all"
