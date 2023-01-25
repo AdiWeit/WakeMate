5a5f4f538c10d2225a7b77110804dfffd25e1722
AdiWeit <43521682+AdiWeit@users.noreply.github.com>
2023-01-25 13:52:16 +0100

    :building_construction:new architecture
    getting settings now in "power_settings_manager.py"

--- cpu_utilization.py
+++ cpu_utilization.py
@@ -1,64 +1,30 @@
 #!/usr/bin/env python3
-import subprocess
 import psutil
-import settings
-import mywake
+from rich import print
+from . import settings
+from . import power_settings_manager
 import threading
-# checks if cpu is "working hard" (cpu% > inhibit_percentage)
 def cpu_working_hard(time_checking, inhibit_percentage):
-  # Calling psutil.cpu_precent() for [time_checking] seconds
+  """"
+  checks if cpu is "working hard" (cpu % > inhibit_percentage)
+  """
+  # checking utilization average the cpu average for [time_checking] seconds
   usage = psutil.cpu_percent(time_checking)
   print("cpu usage:", usage)
-  # update time_inactive (important if user changed it)
-  if list(get_time_inactive()["sleep"].values()) != [0, 0]:
-    global time_inactive
-    time_inactive = get_time_inactive()
+  power_settings_manager.update_time_inactive()
   return usage > inhibit_percentage
 
-time_inactive = settings.default_time_inactive
-# get the current time_inactive (power) settings
-def get_time_inactive():
-  return  {
-    "sleep": {
-      "decharging":
-        # getting the inactivity time unitl the PC goes to sleep when running on battery (not connected to charging cable)
-        get_settings_subprocess('sleep-inactive-battery-timeout'),
-      "charging":
-        # getting the inactivity time unitl the PC goes to sleep when charging (ac connected)
-        get_settings_subprocess('sleep-inactive-ac-timeout')
-      },
-    "screen": {
-      "decharging":
-        # getting the inactivity time unitl the screen goes blank when running on battery (not connected to charging cable)
-        get_settings_subprocess('sleep-display-battery'),
-      "charging":
-        # getting the inactivity time unitl the screen goes blank when charging (ac connected)
-        get_settings_subprocess('sleep-display-ac')
-    },
-  }
-
-def update():
+def update(callback):
+  """
+  checks if the cpu is working hard every [settings.SLEEP_INTERVAL_SECONDS] seconds
+  """
   try:
     while True:
-      print("new run")
-      if cpu_working_hard(settings.SLEEP_INTERVAL_SECONDS, settings.inhibit_percentage):
-        mywake.change_inhibit(True)
-      else:
-        mywake.change_inhibit(False)
+      callback({"type": "cpu working hard", "state": cpu_working_hard(settings.SLEEP_INTERVAL_SECONDS, settings.inhibit_percentage)})
   except KeyboardInterrupt:
     print("exit via key press")
-# thread needed because mywake has to be imported but imports this script
-def start_checking():
-  thread = threading.Thread(target=update)
+# thread needed because of endless loop in update function
+def start_checking(callback):
+  thread = threading.Thread(target=update, args=(callback,))
   thread.daemon = True
   thread.start()
-def get_settings_subprocess(setting):
-  return int(subprocess.run(
-  # command to make this work when running in the background with systemctl 
-  # "adi": user name 
-  # "1000": uid (user id?, possible ids can be found as foldernames at "/run/user"))
-  'export DISPLAY=:0; sudo -H -u adi DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ' + 
-  'dconf read /org/cinnamon/settings-daemon/plugins/power/' + setting, 
-  shell=True,
-  stdout=subprocess.PIPE
-  ).stdout.strip().decode())
